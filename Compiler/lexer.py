e1602d988a2b5f5b5935ed8f4c7d378c3c685234
Joshua McCorkell <joshua@themccorkells.com>
2023-02-23 17:18:02 +1000

    Refactoring Lexer to class, adding parser files.

--- Compiler/lexer.py
+++ Compiler/lexer.py
@@ -25,6 +25,7 @@ from typing import List
 
 quote_types = set(item.value for item in StringDelimiter)
 comment_starters = set(item.start for item in Comment)
+code_delimiters = set(item.value for item in CodeDelimiter)
 
 
 class EOFException(Exception):
@@ -73,210 +74,211 @@ class Token:
     end: int
 
 
-def number(chars: CharStream) -> Token:
-    start, line, token = chars.start_token()
-    has_dot = (token == ".")
-    while True:
-        try:
-            char = chars.peak(1)
-            if char in reg_chars:
-                token += chars.advance_next()
-            elif char in [Operator.PLUS.value, Operator.MINUS.value] and chars.peak(
-                -1
-            ) in ["e", "E"]:
-                token += chars.advance_next()
-            elif char == "." and not has_dot:
-                token += chars.advance_next()
-                has_dot = True
-            else:
-                break
-        except EOFException:
-            break
-    if Formats.is_number(token):
-        return Token(TokenType.NUMBER, token, line, start, chars.current_index - 1)
-    else:
-        raise SyntaxError(f"Invalid Number Literal {token}", line)
-
-
-def word(chars: CharStream) -> Token:
-    start, line, token = chars.start_token()
-    while True:
-        try:
-            char = chars.peak(1)
-            if char in reg_chars:
-                token += chars.advance_next()
-            else:
-                break
-        except EOFException:
-            break
-    end = chars.current_index - 1
-    if token in set(item.value for item in Operator):
-        return Token(TokenType.OPERATOR, Operator(token), line, start, end)
-    elif token in set(item.value for item in Keyword):
-        return Token(TokenType.KEYWORD, Keyword(token), line, start, end)
-    else:
-        return Token(TokenType.IDENTIFIER, token, line, start, end)
-
-
-def delimiter(chars: CharStream) -> Token:
-    start, line, token = chars.start_token()
-    end = chars.current_index - 1
-    return Token(TokenType.CODE_DELIMITER, CodeDelimiter(token), line, start, end)
-
-
-def symbol(chars: CharStream) -> Token:
-    start, line, token = chars.start_token()
-    if token == "." and Formats.is_number((token + chars.peak(1))):
-        return number(chars)
-    while True:
-        try:
-            char = chars.peak(1)
-            if char in symbols:
-                token += chars.advance_next()
-            else:
-                break
-        except EOFException:
-            break
-    end = chars.current_index - 1
-    if token in set(item.value for item in Operator):
-        return Token(TokenType.OPERATOR, Operator(token), line, start, end)
-    elif token in set(item.value for item in Keyword):
-        return Token(TokenType.KEYWORD, Keyword(token), line, start, end)
-    else:
-        raise SyntaxError(f"Invalid Operator/Symbol {token}", line)
-
-
-def comment(comment_type: Comment, chars: CharStream) -> Token:
-    start, line, token = chars.start_token()
-    while True:
-        try:
-            token += chars.advance_next()
-            if token[-1] == comment_type.end:
-                break
-        except EOFException:
-            break
-    return Token(comment_type.token_type, token, line, start, chars.current_index - 1)
-
-
-def string(chars: CharStream) -> Token:
-    _, _, token = chars.start_token()
-    if token == StringDelimiter.PLAIN_STRING.value:
-        return plain_string(chars)
-    elif token == StringDelimiter.FORMAT_STRING.value:
-        return format_string(chars)
-    elif token == StringDelimiter.REGEX_STRING.value:
-        return regex(chars)
-
-
-def regex(chars: CharStream) -> Token:
-    start, line, _ = chars.start_token()
-    token = chars.peak(0)
-    while True:
-        try:
-            char = chars.peak(1)
-            if char == StringDelimiter.REGEX_STRING.value:
-                chars.advance_next()
-                break
-            elif (
-                char == escape_char
-                and chars.peak(2) == StringDelimiter.REGEX_STRING.value
-            ):
-                token += StringDelimiter.REGEX_STRING.value
-            elif char == "/":
-                token += "\/"
-            else:
-                token += chars.advance_next()
-        except EOFException:
-            raise SyntaxError(f"Unterminated Regex Literal {token}", line)
-    current_regex_flags = []
-    while True:
-        try:
-            char = chars.peak(1)
-            if char in REGEX_FLAGS and char not in current_regex_flags:
-                token += chars.advance_next()
-                current_regex_flags.append(token[-1])
-            elif char in letters:
-                raise SyntaxError(f"Invalid Regex Flags: {current_regex_flags}")
-            else:
-                break     
-        except EOFException:
-            break
-    return Token(TokenType.REGEX_STRING, token, line, start, chars.current_index - 1)
-
-
-def plain_string(chars: CharStream) -> Token:
-    start, line, _ = chars.start_token()
-    token = chars.peak(0)
-    while True:
-        try:
-            char = chars.peak(1)
-            if char == StringDelimiter.PLAIN_STRING.value:
-                token += chars.advance_next()
-                break
-            elif char == escape_char:
-                token += chars.advance_next() + chars.advance_next()
-            else:
-                token += chars.advance_next()
-        except EOFException:
-            raise SyntaxError(f"Unterminated String Literal {token}", line)
-    return Token(TokenType.PLAIN_STRING, token, line, start, chars.current_index - 1)
-
-
-def format_string(chars: CharStream) -> Token:
-    # TEMPORARY
-    return plain_string(chars)
-
-
-def lex(code: str) -> List[Token]:
-    try:
-        tokens = []
-        chars = CharStream(code)
+class Lexer:
+    def number(self, chars: CharStream) -> Token:
+        start, line, token = chars.start_token()
+        has_dot = (token == ".")
         while True:
             try:
-                char = chars.advance_next()
+                char = chars.peak(1)
+                if char in reg_chars:
+                    token += chars.advance_next()
+                elif char in [Operator.PLUS.value, Operator.MINUS.value] and chars.peak(
+                    -1
+                ) in ["e", "E"]:
+                    token += chars.advance_next()
+                elif char == "." and not has_dot:
+                    token += chars.advance_next()
+                    has_dot = True
+                else:
+                    break
             except EOFException:
                 break
-            try:
-                char_plus = None
-                char_plus = char + chars.peak(1)
-                char_plus_plus = char_plus + chars.peak(2)
-            except EOFException:
-                if char_plus is not None:
-                    char_plus_plus = char_plus
-                else:
-                    char_plus_plus = char_plus = char
+        if Formats.is_number(token):
+            return Token(TokenType.NUMBER, token, line, start, chars.current_index - 1)
+        else:
+            raise SyntaxError(f"Invalid Number Literal {token}", line)
 
-            if char in digits:
-                tokens.append(number(chars))
-            elif char in quote_types:
-                tokens.append(string(chars))
-            elif char in comment_starters:
-                tokens.append(comment(Comment(char), chars))
-            elif char_plus in comment_starters:
-                tokens.append(comment(Comment(char + chars.peak(1)), chars))
-            elif char_plus_plus in comment_starters:
-                tokens.append(
-                    comment(Comment(char + chars.peak(1) + chars.peak(2)), chars)
-                )
-            elif char in reg_chars:
-                tokens.append(word(chars))
-            elif char in [item.value for item in CodeDelimiter]:
-                tokens.append(delimiter(chars))
-            elif char in symbols:
-                tokens.append(symbol(chars))
-            elif char in whitespace:
-                continue
+
+    def word(self, chars: CharStream) -> Token:
+        start, line, token = chars.start_token()
+        while True:
+            try:
+                char = chars.peak(1)
+                if char in reg_chars:
+                    token += chars.advance_next()
+                else:
+                    break
+            except EOFException:
+                break
+        end = chars.current_index - 1
+        if token in set(item.value for item in Operator):
+            return Token(TokenType.OPERATOR, Operator(token), line, start, end)
+        elif token in set(item.value for item in Keyword):
+            return Token(TokenType.KEYWORD, Keyword(token), line, start, end)
+        else:
+            return Token(TokenType.IDENTIFIER, token, line, start, end) 
+
+
+    def symbol(self, chars: CharStream) -> Token:
+        start, line, token = chars.start_token()
+        if token == "." and Formats.is_number((token + chars.peak(1))):
+            return self.number(chars)
+        if token in code_delimiters:
+            return Token(TokenType.CODE_DELIMITER, CodeDelimiter(token), line, start, chars.current_index - 1)
+        while True:
+            try:
+                char = chars.peak(1)
+                if char in symbols:
+                    token += chars.advance_next()
+                else:
+                    break
+            except EOFException:
+                break
+        end = chars.current_index - 1
+        if token in set(item.value for item in Operator):
+            return Token(TokenType.OPERATOR, Operator(token), line, start, end)
+        elif token in set(item.value for item in Keyword):
+            return Token(TokenType.KEYWORD, Keyword(token), line, start, end)
+        else:
+            raise SyntaxError(f"Invalid Operator/Symbol {token}", line)
+
+
+    def comment(self, comment_type: Comment, chars: CharStream) -> Token:
+        start, line, token = chars.start_token()
+        while True:
+            try:
+                token += chars.advance_next()
+                if token[-1] == comment_type.end:
+                    break
+            except EOFException:
+                break
+        return Token(comment_type.token_type, token, line, start, chars.current_index - 1)
+
+
+    def string(self, chars: CharStream) -> Token:
+        _, _, token = chars.start_token()
+        if token == StringDelimiter.PLAIN_STRING.value:
+            return self.plain_string(chars)
+        elif token == StringDelimiter.FORMAT_STRING.value:
+            return self.format_string(chars)
+        elif token == StringDelimiter.REGEX_STRING.value:
+            return self.regex(chars)
+
+
+    def regex(self, chars: CharStream) -> Token:
+        start, line, _ = chars.start_token()
+        token = chars.peak(0)
+        while True:
+            try:
+                char = chars.peak(1)
+                if char == StringDelimiter.REGEX_STRING.value:
+                    chars.advance_next()
+                    break
+                elif (
+                    char == escape_char
+                    and chars.peak(2) == StringDelimiter.REGEX_STRING.value
+                ):
+                    token += StringDelimiter.REGEX_STRING.value
+                elif char == "/":
+                    token += "\/"
+                else:
+                    token += chars.advance_next()
+            except EOFException:
+                raise SyntaxError(f"Unterminated Regex Literal {token}", line)
+        current_regex_flags = []
+        while True:
+            try:
+                char = chars.peak(1)
+                if char in REGEX_FLAGS and char not in current_regex_flags:
+                    token += chars.advance_next()
+                    current_regex_flags.append(token[-1])
+                elif char in letters:
+                    raise SyntaxError(f"Invalid Regex Flags: {current_regex_flags}")
+                else:
+                    break     
+            except EOFException:
+                break
+        return Token(TokenType.REGEX_STRING, token, line, start, chars.current_index - 1)
+
+
+    def plain_string(self, chars: CharStream) -> Token:
+        start, line, _ = chars.start_token()
+        token = chars.peak(0)
+        while True:
+            try:
+                char = chars.peak(1)
+                if char == StringDelimiter.PLAIN_STRING.value:
+                    token += chars.advance_next()
+                    break
+                elif char == escape_char:
+                    token += chars.advance_next() + chars.advance_next()
+                else:
+                    token += chars.advance_next()
+            except EOFException:
+                raise SyntaxError(f"Unterminated String Literal {token}", line)
+        return Token(TokenType.PLAIN_STRING, token, line, start, chars.current_index - 1)
+
+
+    def format_string(self, chars: CharStream) -> Token:
+        # TEMPORARY
+        return self.plain_string(chars)
+
+
+    def lex(self, *, code_string: str="", char_stream: CharStream=None) -> List[Token]:
+        try:
+            tokens = []
+            if char_stream is None:
+                chars = CharStream(code_string)
             else:
-                raise SyntaxError(f"Invalid Character {char}", chars.line_number)
-        return tokens
-    except SyntaxError as e:
-        tokens.append(e)
-        return tokens
+                chars = char_stream
+            while True:
+                try:
+                    char = chars.advance_next()
+                except EOFException:
+                    break
+                try:
+                    char_plus = None
+                    char_plus = char + chars.peak(1)
+                    char_plus_plus = char_plus + chars.peak(2)
+                except EOFException:
+                    if char_plus is not None:
+                        char_plus_plus = char_plus
+                    else:
+                        char_plus_plus = char_plus = char
+
+                if char in digits:
+                    tokens.append(self.number(chars))
+                elif char in quote_types:
+                    tokens.append(self.string(chars))
+                elif char in comment_starters:
+                    tokens.append(self.comment(Comment(char), chars))
+                elif char_plus in comment_starters:
+                    tokens.append(self.comment(Comment(char + chars.peak(1)), chars))
+                elif char_plus_plus in comment_starters:
+                    tokens.append(
+                        self.comment(Comment(char + chars.peak(1) + chars.peak(2)), chars)
+                    )
+                elif char in reg_chars:
+                    tokens.append(self.word(chars))
+                elif char in symbols:
+                    tokens.append(self.symbol(chars))
+                elif char in whitespace:
+                    continue
+                else:
+                    raise SyntaxError(f"Invalid Character {char}", chars.line_number)
+            return tokens
+        except SyntaxError as e:
+            tokens.append(e)
+            return tokens
+
+
+def command_line():
+    import sys
+    code = open(sys.argv[1]).read()
+    print(code)
+    print("\n", *Lexer().lex(code_string=code), sep="\n")
 
 
 if __name__ == "__main__":
-    import sys
-
-    code = open(sys.argv[1]).read()
-    print(code)
-    print("\n", *lex(code), sep="\n")
+    command_line()
---
+++ Compiler/parser.py
@@ -0,0 +1,2 @@
+from lexer import Lexer
+from syntax_error import SyntaxError
